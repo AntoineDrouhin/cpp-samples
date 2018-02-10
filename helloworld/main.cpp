cdbf67f828184f2b83411fb624ab2e82dba96515
Antoine Drouhin <antoine.drouhin@gmail.com>
2018-02-10 17:37:21 +0100

    A nice HelloWorld in C++

---
+++ helloworld/main.cpp
@@ -0,0 +1,17 @@
+//hello.cpp
+
+/*******
+Basic Hello World,
+
+Display Hello World to the screen
+*******/
+
+#include <cstdio>
+using namespace std;
+
+int main( int argc, char ** argv )
+{
+    const char * str = "Hello World";
+    puts(str);
+    return 0;
+}
