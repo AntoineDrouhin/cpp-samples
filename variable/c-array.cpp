bf7f5f79979a66483229be037e65be14b2ecb7a3
Antoine Drouhin <antoine.drouhin@gmail.com>
2018-02-11 12:18:22 +0100

    c-array

---
+++ variable/c-array.cpp
@@ -0,0 +1,38 @@
+// c-array and c-string are fundamental types in C++
+// They are very different from their corresponding stl classes.
+
+#include <cstdio>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    int ia[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        ia[i] = i + 1;
+        printf("ia[%d] = %d\n", i, ia[i]);
+    }
+
+    printf("\n");
+
+    int ia2[5] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("ia2[%d] = %d\n", i, ia2[i]);
+    }
+
+    printf("\n");
+
+    int ia3[5];
+    int *ip3 = ia3;
+
+    for (int i = 0; i < 5; i++){
+        *(ip3 + i)  = i + 1;
+        printf("ia3[%d] = %d\n", i, *(ia3 + i));
+    }
+
+
+}
